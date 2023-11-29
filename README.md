120ba4bcf7697d38e977b1f1c07858007e47dac0
ZaidMadanat <zaidsmadanat@gmail.com>
2023-11-28 16:08:42 -0800

    finished script, project complete

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Etch-a-Sketch
-Part of The Odin Projects' projects.
+Welcome to Etch-a-Sketch. 
+You are to erase, draw in black, or random rainbow colors. 
+You also have the ability to resize the grid as you like.
--- script.js
+++ script.js
@@ -1,25 +1,14 @@
 const grid = document.querySelector('.grid');
+let startdrawing = false; 
+let color = 'black';
+let size = 16;
 
-function CreateGrid() { 
-    /*
-    for (let j = 0; j < 2; j++) { 
-        for (let i = 0; i < 16; i++) { 
-            const row_square = document.createElement('button'); 
-            row_square.classList.add('square_style'); 
-
-            const column_square = document.createElement('button'); 
-            column_square.classList.add('square_style'); 
-
-            row.appendChild(row_square);
-            column.appendChild(column_square); 
-        }
-    }
-    */
-    for (let i = 0; i < 16; i++ ) { 
+function createGrid(size) { 
+    for (let i = 0; i < size; i++ ) { 
         const column = document.createElement('div');
         column.classList.add('col_style'); 
         grid.appendChild(column);
-        for (let j = 0; j < 16; j++) { 
+        for (let j = 0; j < size; j++) { 
             const square = document.createElement('button'); 
             square.classList.add('square_style'); 
             column.appendChild(square);
@@ -28,4 +17,61 @@ function CreateGrid() {
 
 }
 
-CreateGrid();
+function updateSize(newSize) { 
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+    
+    createGrid(newSize);
+    draw();
+}
+
+function getSize() { 
+    const gridsize = document.getElementById('myRange');
+    const displaysize = document.querySelector('.value'); 
+    gridsize.oninput = function() { 
+        displaysize.innerHTML = this.value; 
+        updateSize(this.value);
+    } 
+}
+
+function toggleDrawing() { 
+    startdrawing = !startdrawing; 
+}
+
+function setcolor(e) { 
+    if(startdrawing) { 
+        if (color === 'rainbow') { 
+            e.target.style.backgroundColor = `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`;
+        }
+        else { 
+            e.target.style.backgroundColor = color;
+        }
+    }
+}
+
+function draw() {  
+    const erase = document.querySelector("#erase"); 
+    const black = document.querySelector("#black"); 
+    const rainbow = document.querySelector("#rainbow");
+    const squares = document.querySelectorAll('.square_style'); 
+    
+    black.addEventListener('click', function(e) { 
+        color = 'black';
+    })
+    rainbow.addEventListener('click', function(e) { 
+        color = 'rainbow'; 
+    })
+    erase.addEventListener('click', function(e) { 
+        color = 'white';
+    })
+
+    squares.forEach(square => { 
+        square.addEventListener('mouseover', setcolor); 
+        square.addEventListener('click', toggleDrawing);
+    });
+}
+
+createGrid(size)
+draw();
+getSize();
